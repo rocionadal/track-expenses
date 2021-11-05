d6c56f00a9f1694970b53987ccc208de75dfcc8f
rocionadal <rocioonadal@gmail.com>
2021-11-05 16:05:42 +0100

    New Card component to avoid code duplication

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,15 +1,16 @@
-import ExpenseDate from './ExpensesDate';
+import ExpenseDate from "./ExpensesDate";
+import Card from "./Card";
 import "./ExpensesItem.css";
 
 function ExpensesItem(props) {
   return (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
