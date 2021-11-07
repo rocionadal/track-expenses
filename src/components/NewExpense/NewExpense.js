0baf530f38fae213d78c98564e009d6ddd6341cd
rocionadal <rocioonadal@gmail.com>
2021-11-07 22:09:38 +0100

    Add logic to render Form conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
+
 import './NewExpense.css';
 
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   function saveExpenseDataHandler(enteredExpenseData) {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString,
     };
-
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
