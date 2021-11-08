df9625441135cb43d193da7f978d796072877038
rocionadal <rocioonadal@gmail.com>
2021-11-08 21:52:13 +0100

    Add chart + small details

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+
 import ExpenseItem from './ExpensesItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2021');
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   function filterChangeHandler(selectedYear) {
     setFilteredYear(selectedYear);
@@ -36,6 +37,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         {expensesContent}
       </Card>
     </div>
